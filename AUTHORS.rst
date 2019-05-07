c992fe474b9219a57f32037f401ec8c45ed4f48f
Yalun Yu <alanyu@de10.lobos.nih.gov>
2019-05-07 12:34:25 -0400

    initial commit for adding mis files

---
+++ AUTHORS.rst
@@ -0,0 +1,28 @@
+
+
+History
+-------
+
+FFLOP was initiated by Yalun in the group of Jeff Klauda
+at University of Maryland, College Park, MD, USA in 2019.
+
+FFLOP's concept and vision were equally contributed (in alphabetical order)
+by Jeffery Klauda, Andreas Krämer, and Yalun Yu.
+
+Development Lead
+----------------
+
+* Yalun Yu, UMD, College Park, MD, USA (yalun.research@gmail.com)
+
+
+
+Core Developers
+---------------
+
+* Andreas Krämer, NIH NHLBI, Bethesda, MD, USA 
+
+
+Contributors
+------------
+
+Not yet.
---
+++ setup.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+import versioneer
+import sys
+
+# Custom commands for versioning
+cmdclass = versioneer.get_cmdclass()
+
+# Custom commands for building the docs
+try:
+    from sphinx.setup_command import BuildDoc
+    cmdclass['build_sphinx'] = BuildDoc
+except ImportError:
+    print("Could not import sphinx. build_sphinx command not installed.")
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+
+requirements = [
+    'numpy>=1.12',
+    'pandas',
+    'nbsphinx', # sphinx documentation for jupyter notebooks
+    'sphinx_click', # sphinx documentation for click
+    'alabaster', # alabaster theme for documentation
+    'packaging',
+    'scipy>=1.0.0',
+    'openmm>=7.3.0',
+    # put package requirements here
+]
+
+if sys.version_info[0] < 3:
+    requirements.append('matplotlib<3')
+else:
+    requirements.append('matplotlib')
+
+setup_requirements = [
+    'pytest-runner',
+    #  put setup requirements (distutils extensions, etc.) here
+]
+
+test_requirements = [
+    'pytest>=3.4',
+    # put package test requirements here
+]
+
+setup(
+    name='fflop',
+    version=versioneer.get_version(),
+    description="Force Field of Lipids Optimization Package",
+    long_description=readme + '\n\n' + history,
+    author="Yalun Yu",
+    author_email='yalun.research@gmail.com',
+    url='https://github.com/alanyu19/fflop',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'fflop=fflop.cli:main'
+        ]
+    },
+    include_package_data=True,
+    install_requires=requirements,
+    license="GNU General Public License v3",
+    zip_safe=False,
+    keywords='fflop',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+    cmdclass=cmdclass,
+)
