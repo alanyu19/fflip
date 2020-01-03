07fd9852626f99a1526756e59a673517cf7a83f2
YALUN YU <alanyu@yalun-mac.fios-router.home>
2020-01-03 13:20:04 -0500

    bug fix

--- fflip/ljpme/moreutil.py
+++ fflip/ljpme/moreutil.py
@@ -117,9 +117,21 @@ def smooth(y, box_pts):
     return y_smooth
 
 
-def find_peak_position(x, first_n_peak = 2):
+def combine_peak_foot_indexes(peak_indexes, foot_indexes):
+    assert 0 <= len(peak_indexes) - len(foot_indexes) <= 1
+    i = 0
+    comb = []
+    while i < len(peak_indexes) and i < len(foot_indexes):
+        comb.append(peak_indexes[i])
+        i += 1
+    if i < len(peak_indexes):
+        comb.append(i)
+    return comb
+
+
+def find_peak_position(x, first_n_peak=2):
     local_max_bool = np.r_[True, x[1:] > x[:-1]] & np.r_[x[:-1] > x[1:], True]
-    _where_max = np.where(local_max_bool == True)
+    _where_max = np.where(local_max_bool==True)
     _where_big = np.where(x > 0.2)
     where_max = list(_where_max[0])
     where_big = list(_where_big[0])
@@ -136,9 +148,9 @@ def find_peak_position(x, first_n_peak = 2):
     return return_list
 
 
-def find_foot_position(x, first_n_foots = 1):
+def find_foot_position(x, first_n_foots=1):
     local_min_bool = np.r_[True, x[1:] < x[:-1]] & np.r_[x[:-1] < x[1:], True]
-    _where_min = np.where(local_min_bool == True)
+    _where_min = np.where(local_min_bool==True)
     _where_big = np.where(x > 0.05)
     where_min = list(_where_min[0])
     where_big = list(_where_big[0])
@@ -179,40 +191,43 @@ def find_rdf_peaks_and_foots(rdf, r=None, first_n_peaks=2, first_n_foots=1,
         r_list = []
         for i in range(int(shape_original[0])):
             copy_of_rdf = rdf[i, :]
-            peak_indexes_smoothed = find_peak_position(
+            peak_indexes_smd = find_peak_position(
                 smooth(copy_of_rdf, smooth_window_size), first_n_peaks
             )
-            foot_indexes_smoothed = find_foot_position(
+            foot_indexes_smd = find_foot_position(
                 smooth(copy_of_rdf, smooth_window_size), first_n_foots
             )
 
-            peak_indexes = find_peak_position(copy_of_rdf, first_n_peaks)
-            foot_indexes = find_foot_position(copy_of_rdf, first_n_foots)
+            # peak_indexes = find_peak_position(copy_of_rdf, first_n_peaks)
+            # foot_indexes = find_foot_position(copy_of_rdf, first_n_foots)
+            indexes = combine_peak_foot_indexes(
+                peak_indexes_smd, foot_indexes_smd
+            )
             if not no_r:
                 r_list.append(
-                    r[np.array(peak_indexes_smoothed + foot_indexes_smoothed)]
+                    r[np.array(indexes)]
                 )
             value_list.append(
-                copy_of_rdf[np.array(peak_indexes + foot_indexes)]
+                copy_of_rdf[np.array(indexes)]
             )
         if not no_r:
             return r_list, value_list
         else:
             return value_list
     else:
-        peak_indexes_smoothed = find_peak_position(
+        peak_indexes_smd = find_peak_position(
             smooth(rdf, smooth_window_size), first_n_peaks
         )
-        foot_indexes_smoothed = find_foot_position(
+        foot_indexes_smd = find_foot_position(
             smooth(rdf, smooth_window_size), first_n_foots
         )
-        peak_indexes = find_peak_position(rdf, first_n_peaks)
-        foot_indexes = find_foot_position(rdf, first_n_foots)
+        # peak_indexes = find_peak_position(rdf, first_n_peaks)
+        # foot_indexes = find_foot_position(rdf, first_n_foots)
+        indexes = combine_peak_foot_indexes(peak_indexes_smd, foot_indexes_smd)
         if not no_r:
-            return r[np.array(peak_indexes_smoothed + foot_indexes_smoothed)], \
-                rdf[np.array(peak_indexes + foot_indexes)]
+            return r[np.array(indexes)], rdf[np.array(indexes)]
         else:
-            return rdf[np.array(peak_indexes + foot_indexes)]
+            return rdf[np.array(indexes)]
 
 
 def extract_exp(name, exp):
