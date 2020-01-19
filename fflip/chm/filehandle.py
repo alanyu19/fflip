75a894c611e53d2653fe95169b78b08774cdec6c
Yalun Yu <alanyu@m3.lobos.nih.gov>
2020-01-19 18:01:08 -0500

    chm filehandle

--- fflip/chm/filehandle.py
+++ fflip/chm/filehandle.py
@@ -10,7 +10,8 @@ def replace_top_parameter(top_file_template, para_dict, write_to_new_dir=None):
     else:
         assert os.path.isdir(write_to_new_dir)
         os.system('cp {} {}'.format(top_file_template, write_to_new_dir))
+        new_file = os.path.join(write_to_new_dir, top_file_template.split('/')[-1])
         replace_top_parameter(
-            os.path.join(write_to_new_dir, top_file_template),
+            new_file,
             para_dict=para_dict
         )
