942e5b4a982682f1e3adc9dd2247197081d2037b
Yalun Yu <alanyu@m3.lobos.nih.gov>
2020-01-21 20:24:06 -0500

    file handling

--- fflip/chm/filehandle.py
+++ fflip/chm/filehandle.py
@@ -19,16 +19,32 @@ def replace_top_parameter(top_file_template, para_dict, write_to_new_dir=None):
 
 
 def torfix_to_str(torfixes, str_file):
-    with open(str_file, 'w') as str:
+    with open(str_file, 'w') as chmstr:
+        chmstr.write(
+            """* foo
+*
+read param card flex append
+
+DIHEDRALS\n"""
+        )
         for tfix in torfixes:
             atom1 = tfix.atom1
             atom2 = tfix.atom2
             atom3 = tfix.atom3
             atom4 = tfix.atom4
             value_dict = tfix.values
-            for key, value in zip(value_dict.keys, value_dict.values):
-                str.write(
-                    atom1.upper(), atom2.upper(), atom3.upper(), atom4.upper(),
-                    str(key), str(value[0]), str(value[1])
+            for key, value in zip(value_dict.keys(), value_dict.values()):
+                chmstr.write(
+                    "{0:<7s}\t{1:<7s}\t{2:<7s}\t{3:<7s}\t{4:>7.4f}\t{5:>5d}\t{6:>6.1f}\n".format(
+                        atom1.upper(), atom2.upper(),
+                        atom3.upper(), atom4.upper(),
+                        round((value[1]/4.184), 3),
+                        int(key),
+                        round((180*value[0]/3.141592653589793), 1)   
+                    )
                 )
+        chmstr.write(
+            """END
+RETURN"""
+        )
 
