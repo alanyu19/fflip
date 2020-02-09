27a097aff95ae65a635c2b7b4a6b511c8e2b96a9
Yalun Yu <alanyu@m3.lobos.nih.gov>
2020-02-09 10:33:00 -0500

    dbscan related

--- fflip/analysis/dbscan.py
+++ fflip/analysis/dbscan.py
@@ -14,7 +14,8 @@ Nframe = 1
 # Find line number for (x_pt, y_pt)
 def res_info(x_pt, y_pt, block_data):
     for entry in block_data:
-        if str(x_pt) in entry and str(y_pt) in entry:
+        if "{:.5f}".format(float(x_pt)) == "{:.5f}".format(float(entry[3])) and \
+            "{:.5f}".format(float(y_pt)) == "{:.5f}".format(float(entry[4])):
             resn = entry[0]
             resid = entry[1]
             nim = entry[2]
@@ -22,12 +23,12 @@ def res_info(x_pt, y_pt, block_data):
 
 
 def cluster_calculation(
-        Dcut, index_trj, leaflet, nlip, nframe,
+        Dcut, index_trj, leaflet, nlip, nframe, minsp=3,
         box_file_template="{}/box-{}.txt", xy_file_temlate="{}/xy-{}.dat",
-        num_file_template="num-{}.txt", tot_file_template="tot-{}.txt"
+        num_file_template="{}/num-{}.txt", tot_file_template="{}/tot-{}.txt"
 ):
     print(
-        'line\tIdyn\tIf\tIc\tIlip\tResn\tResid\tNimg\tX_coord\tY_coord'
+        'line\tIdyn\tIf\tIc\tIlip\tResn\tResid\tNimg\tX_coord\t    Y_coord'
     )
 
     # Box data
@@ -45,43 +46,45 @@ def cluster_calculation(
     for fr in range(nframe):
         info1, info2 = cluster_bridge(
             Data, left_all_frame[fr], right_all_frame[fr],
-            Dcut, index_trj, fr, nlip
+            Dcut, index_trj, fr, nlip, minsp=minsp
         )
         if info1 is not None:
             info1s.append(info1)
-        if info2 is not None:
-            info2s.append(info2)
-    with open(num_file_template.format(index_trj), 'w') as f:
+            info2s += info2
+    file_to_write = num_file_template.format(leaflet, index_trj)
+    with open(file_to_write, 'w') as f:
         # Write title, index of frame, Index of cluster,
         # number of lipids in each cluster
         f.write('%s\t%s\t%s\t%s\n' % ('Idyn', 'If2', 'Ic', 'Nlc'))
         for string in info2s:
             f.write(string)
-    with open(tot_file_template.format(index_trj), 'w') as f:
+    file_to_write = tot_file_template.format(leaflet, index_trj)
+    with open(file_to_write, 'w') as f:
         f.write('%s\t%s\t%s\t%s\t%s\n' % ('Idyn', 'If1', 'Nlf', 'Ncf', 'Nlca'))
         for string in info1s:
             f.write(string)
 
 
-def cluster_bridge(Data, left, right, Dcut, index_trj, frame, nlip):
+def cluster_bridge(Data, left, right, Dcut, index_trj, frame, nlip, minsp=3):
     Idyn = index_trj
     lpf = nlip * 9  # lines per frame
     Fr = int(frame)  # starts from 0
     Data = Data[Fr * lpf:(Fr + 1) * lpf]
+    data_ = Data[:, 3:]
     data = Data[:, 3:].astype(float)
     # Find number of rows and columns
     # NR = np.shape(data)[0]
     # NC = np.shape(data)[1]
 
     # calculte clustering of single block
-    info1, info2 = run_dbscan(data, Data, Idyn, Dcut, left, right, Fr)
+    info1, info2 = run_dbscan(data, data_, Data, Idyn, Dcut, left, right, Fr, minsp=minsp)
     return info1, info2
 
 
-def run_dbscan(data, Data, Idyn, Dcut, left, right, frame, fnum, ftot):
+def run_dbscan(data, data_, Data, Idyn, Dcut, left, right, frame, minsp=3):
     line = 0
     # Compute DBSCAN
-    db = DBSCAN(eps=Dcut, min_samples=3).fit(data)
+    db = DBSCAN(eps=Dcut, min_samples=minsp).fit(data)
     # print db.get_params(deep=True)
     labels = db.labels_
     # components = db.components_
@@ -97,6 +100,7 @@ def run_dbscan(data, Data, Idyn, Dcut, left, right, frame, fnum, ftot):
         # loop over clusters
         for i in range(0, n_clusters_, 1):
             comp_1 = data[labels == i]
+            comp_1_ = data_[labels == i]
             check1 = any(
                 left < xy[0] < right and right > xy[1] > left for xy in comp_1
             )
@@ -133,12 +137,13 @@ def run_dbscan(data, Data, Idyn, Dcut, left, right, frame, fnum, ftot):
                             comp_1c[j][0], '\t', comp_1c[j][1]
                         )
                         line += 1
-
-                    raise Exception(
-                        "Error! Please use lower Dcut! \n"
-                        "Resid", Lresid_1c[i], "is duplicated", Occ,
+                    print(
+                        "Resid", Lresid_1c[q], "is duplicated", Occ,
                         "times in a cluster for Idyn(dyn index)=", Idyn - 1,
                         ", If(frame index)=", frame, ", Ic(cluster index)=", i
+                    )
+                    raise Exception(
+                        "Error! Please use lower Dcut! \n"
                     )
             Lresid_ac.append(Lresid_1c)
 
@@ -156,31 +161,24 @@ def run_dbscan(data, Data, Idyn, Dcut, left, right, frame, fnum, ftot):
         # and frame averaged cluster size
         Sc = sum(len(x) for x in clusters_2)
         Nc = len(clusters_2)
-        # with open(ftot, 'w') as f:
-        #    f.write(
-        #        '%i\t%i\t%i\t%i\t%2.4f\n' % (
-        #            Idyn - 1, frame, Sc, Nc, Sc / float(Nc)
-        #        )
-        #    )
         return_info_1 = '%i\t%i\t%i\t%i\t%2.4f\n' % (
             Idyn - 1, frame, Sc, Nc, Sc / float(Nc)
         )
-    return_info_2 = None
+    return_info_2 = []
     if len(clusters_2) > 0 and frame >= 0:
         for i in range(0, len(clusters_2), 1):
             comp_f = clusters_2[i]
             # save frame number and number of clusters
-            return_info_2 = '%s\t%s\t%s\t%s\n' % ('Idyn', 'If2', 'Ic', 'Nlc')
+            return_info_2.append('%s\t%s\t%s\t%s\n' % (Idyn-1, frame, i, len(comp_f)))
             for j in range(0, len(comp_f), 1):
                 # Find line in the coordinate file
                 resn, resid, nim = res_info(comp_f[j][0], comp_f[j][1], Data)
                 # print line, frame, cluster, component and x y coordinate
                 # in the cluster
                 print(
-                    line, '\t', Idyn - 1, '\t', frame, '\t', i, '\t', j, '\t',
-                    resn, '\t', resid, '\t', nim, '\t', comp_f[j][0], '\t',
-                    resn, '\t', resid, '\t', nim, '\t', comp_f[j][0], '\t',
-                    comp_f[j][1]
+                    "{0:<8d}{1:<8d}{2:<8d}{3:<8d}{4:<8d}{5:<8s}{6:<8s}{7:<8s}{8:<12f}{9:<12f}".format(
+                        line, Idyn-1, frame, i, j, resn, resid, nim, comp_f[j][0], comp_f[j][1]
+                    )
                 )
                 line += 1
     return return_info_1, return_info_2
--- fflip/chm/util.py
+++ fflip/chm/util.py
@@ -4,13 +4,14 @@ from fflip.chm.lipids.dppc import *
 from fflip.chm.lipids.prpc import *
 from fflip.chm.lipids.dmpc import *
 from fflip.chm.lipids.dopc import *
+from fflip.chm.lipids.pc import *
 from fflip.chm.lipids.pe import *
 
 
 def parse_lipid(lipname):
-    if lipname.lower() in ['dlpc', 'dppc', 'dmpc']:
-        return dppc
+    if lipname.lower() in ['dlpc', 'dppc', 'dmpc', 'popc']:
+        return pc
     if lipname.lower() == 'prpc':
-        return prpc
+        return pc
     if lipname.lower() in ['dppe', 'dope', 'dlpe', 'pope']:
         return pe
