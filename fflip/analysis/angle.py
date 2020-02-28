ac098ef735b79468589798e1451857c167b56af5
YALUN YU <alanyu@yalun-mac.fios-router.home>
2020-02-28 00:34:09 -0500

    test code for tilt angle

--- fflip/analysis/angle.py
+++ fflip/analysis/angle.py
@@ -1,183 +1,84 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-from fflip.analysis.util import (
-    construct_scd_bond, gen_scd_pairs, hard_up_low_atoms
-)
+from fflip.analysis.util import hard_up_low_atoms
 
 
-class OrderParameterFactory(object):
+class AngleDistribution(object):
     """
     A tool that generate order parameter calculations automatically for a
     given topology
     """
-    def __init__(self, topology, special_carbons_for_splitting={},
-                 sep_leaflet=False, recipe=None):
+    def __init__(self, topology, first_vector, constant_second_vector=True,
+                 second_vector=[0, 0, 1], sep_leaflet=False, recipe=None):
         """
         Args:
             topology: the mdtraj topology
-            special_carbons_for_splitting: carbon names for splitting
+            first_vector: the atom pair of the vector
+            second_vector: can be a constant vector or another atom pair
         """
-        self.topology = topology
-        self.scd_res_dict = construct_scd_bond(topology)
-        self.bonds_for_residues = construct_scd_bond(topology)
-        self.scd_pairs_for_residues = gen_scd_pairs(
-            self.bonds_for_residues,
-            special_carbons_for_residues=special_carbons_for_splitting
-        )
-        self.sep_leaflet = sep_leaflet
-        self.recipe = recipe
-
-    def __call__(self):
-        """
-        Returns: all OrderParameterCalculation(s)
-        """
-        calculations = []
-        i = 0
-        for residue in self.scd_pairs_for_residues:
-            for atom_pair in self.scd_pairs_for_residues[residue]:
-                i += 1
-                opc = OrderParameterCalculation(
-                    self.topology, residue, atom_pair[0], atom_pair[1],
-                    sep_leaflet=self.sep_leaflet, recipe=self.recipe
-                )
-                calculations.append(opc)
-        return calculations
-
-    def __iter__(self):
-        """
-        Returns: OrderParameterCalculation(s)
-        """
-        i = 0
-        for residue in self.scd_pairs_for_residues:
-            for atom_pair in self.scd_pairs_for_residues[residue]:
-                i += 1
-                opc = OrderParameterCalculation(
-                    self.topology, residue, atom_pair[0], atom_pair[1]
-                )
-                opc.i = i
-                yield opc
-
-    def __len__(self):
-        length = 0
-        for residue in self.scd_pairs_for_residues:
-            for _ in self.scd_pairs_for_residues[residue]:
-                length += 1
-        return length
-
-
-class OrderParameterCalculation(object):
-    """
-    Calculate the order parameter (average over all molecules) of a (group
-    of) chemical bond
-    TODO: THE BILAYER NORMAL DIRECTION
-    """
-    def __init__(self, topology, residue, atom1, atom2,
-                 sep_leaflet=False, recipe=None):
-        """
-        Args:
-            topology: a topology object of mdtraj
-            residue: str, the residue name
-            atom1: str, the center atom name
-            atom2: the list of hydrogens
-        """
+        self.atom1 = first_vector[0]
+        self.atom2 = first_vector[1]
+        self.constant_second_vector = constant_second_vector
+        if not constant_second_vector:
+            self.atom3 = second_vector[0]
+            self.atom4 = second_vector[1]
+        else:
+            self.constant_vector = second_vector
         self.n_frames = 0.0
-        self.scd = 0.0
+        self.distribution = 0.0
         self.topology = topology
-        self.residue = residue
-        self.atom1 = atom1
-        self.atom2 = atom2
         self.sep_leaflet = sep_leaflet
         self.recipe = recipe
-        if self.sep_leaflet:
-            self.scd_up = 0.0
-            self.scd_low = 0.0
+        self.sele1 = self.topology.select(self.atom1)
+        self.sele2 = self.topology.select(self.atom2)
+        if not self.sep_leaflet:
+            pass
+        else:
+            self.sele1_up, self.sele1_low = hard_up_low_atoms(
+                self.sele1, self.recipe
+            )
+            self.sele2_up, self.sele2_low = hard_up_low_atoms(
+                self.sele2, self.recipe
+            )
+        if not constant_second_vector:
+            self.sele3 = self.topology.select(self.atom3)
+            self.sele4 = self.topology.select(self.atom4)
+            if not self.sep_leaflet:
+                pass
+            else:
+                self.sele3_up, self.sele3_low = hard_up_low_atoms(
+                    self.sele3, self.recipe
+                )
+                self.sele4_up, self.sele4_low = hard_up_low_atoms(
+                    self.sele4, self.recipe
+                )
 
     def __call__(self, traj):
         if not self.sep_leaflet:
-            sele1 = self.topology.select(
-                "resname {} and name {}".format(self.residue, self.atom1)
-            )
-            center_atom = traj.xyz[:, sele1]
-            for i, atom in enumerate(self.atom2):
-                sele2 = self.topology.select(
-                    "resname {} and name {}".format(self.residue, atom)
-                )
-                the_other_atom = traj.xyz[:, sele2]
-                vectors = the_other_atom - center_atom
-                if i == 0:
-                    scd = vectors[:, :, 2] * vectors[:, :, 2] / (
-                        vectors[:, :, 0] * vectors[:, :, 0] +
-                        vectors[:, :, 1] * vectors[:, :, 1] +
-                        vectors[:, :, 2] * vectors[:, :, 2]
-                    )
-                else:
-                    scd += vectors[:, :, 2] * vectors[:, :, 2] / (
-                        vectors[:, :, 0] * vectors[:, :, 0]
-                        + vectors[:, :, 1] * vectors[:, :, 1]
-                        + vectors[:, :, 2] * vectors[:, :, 2]
-                    )
-            scd = scd / (i + 1)
-
-            # update the average scd
-            self.scd = self.n_frames * self.scd + \
-                (-1.5 * np.mean(scd) + 0.5) * traj.n_frames
-            self.n_frames += traj.n_frames
-            self.scd /= self.n_frames
-
-            # return the average scd for every frame
-            return -1.5 * np.mean(scd, axis=1) + 0.5
+            atom1 = traj.xyz[:, self.sele1]
+            atom2 = traj.xyz[:, self.sele2]
+            vector1 = atom1 - atom2
+            if self.constant_second_vector:
+                vector2 = self.constant_vector  # duplicated by number of frames
+            else:
+                atom3 = traj.xyz[:, self.sele3]
+                atom4 = traj.xyz[:, self.sele4]
+                vector2 = atom3 - atom4
+            return vector1, vector2
+            # normalize all the vectors and do the inner product
+            #self.distribution = self.n_frames * self.distribution + \
+            #           distribution * traj.n_frames
+            #self.n_frames += traj.n_frames
+            #self.distribution /= self.n_frames
         else:
-            assert self.recipe is not None
-            sele1 = self.topology.select(
-                "resname {} and name {}".format(self.residue, self.atom1)
-            )
-            sele1_up, sele1_low = hard_up_low_atoms(sele1, self.recipe)
-            center_atom_up = traj.xyz[:, sele1_up]
-            center_atom_low = traj.xyz[:, sele1_low]
-            for i, atom in enumerate(self.atom2):
-                sele2 = self.topology.select(
-                    "resname {} and name {}".format(self.residue, atom)
-                )
-                sele2_up, sele2_low = hard_up_low_atoms(sele2, self.recipe)
-                other_atom_up = traj.xyz[:, sele2_up]
-                other_atom_low = traj.xyz[:, sele2_low]
-                vectors_up = other_atom_up - center_atom_up
-                vectors_low = other_atom_low - center_atom_low
-                if i == 0:
-                    scd_up = vectors_up[:, :, 2] * vectors_up[:, :, 2] / (
-                        vectors_up[:, :, 0] * vectors_up[:, :, 0] +
-                        vectors_up[:, :, 1] * vectors_up[:, :, 1] +
-                        vectors_up[:, :, 2] * vectors_up[:, :, 2]
-                    )
-                    scd_low = vectors_low[:, :, 2] * vectors_low[:, :, 2] / (
-                        vectors_low[:, :, 0] * vectors_low[:, :, 0] +
-                        vectors_low[:, :, 1] * vectors_low[:, :, 1] +
-                        vectors_low[:, :, 2] * vectors_low[:, :, 2]
-                    )
-                else:
-                    scd_up += vectors_up[:, :, 2] * vectors_up[:, :, 2] / (
-                            vectors_up[:, :, 0] * vectors_up[:, :, 0] +
-                            vectors_up[:, :, 1] * vectors_up[:, :, 1] +
-                            vectors_up[:, :, 2] * vectors_up[:, :, 2]
-                    )
-                    scd_low += vectors_low[:, :, 2] * vectors_low[:, :, 2] / (
-                            vectors_low[:, :, 0] * vectors_low[:, :, 0] +
-                            vectors_low[:, :, 1] * vectors_low[:, :, 1] +
-                            vectors_low[:, :, 2] * vectors_low[:, :, 2]
-                    )
-            scd_up = scd_up / (i + 1)
-            scd_low = scd_low / (i + 1)
-
-            # update the average scd
-            self.scd_up = self.n_frames * self.scd_up + \
-                (-1.5 * np.mean(scd_up) + 0.5) * traj.n_frames
-            self.scd_low = self.n_frames * self.scd_low + \
-                (-1.5 * np.mean(scd_low) + 0.5) * traj.n_frames
-            self.n_frames += traj.n_frames
-            self.scd_up /= self.n_frames
-            self.scd_low /= self.n_frames
-
-            # return the average scd for every frame
-            return -1.5 * np.mean(scd_up, axis=1) + 0.5, -1.5 * np.mean(
-                scd_low, axis=1) + 0.5
+            atom1_up = traj.xyz[:, self.sele1_up]
+            atom2_up = traj.xyz[:, self.sele2_up]
+            atom1_low = traj.xyz[:, self.sele1_low]
+            atom2_low = traj.xyz[:, self.sele2_low]
+            vector1_up = atom1_up - atom2_up
+            vector1_low = atom1_low - atom2_low
+            if self.constant_second_vector:
+                vector2_up = self.constant_second_vector
+                vector2_low = self.constant_second_vector
+            # normalize and do inner product
