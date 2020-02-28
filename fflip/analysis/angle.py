c356f231893e2fe0159f28cf276ac0a3991c6bda
YALUN YU <alanyu@yalun-mac.local>
2020-02-28 11:41:27 -0500

    initial code for tilt angle

--- fflip/analysis/angle.py
+++ fflip/analysis/angle.py
@@ -4,19 +4,42 @@ import numpy as np
 from fflip.analysis.util import hard_up_low_atoms
 
 
+def unit_vector(vector):
+    """ Returns the unit vector of the vector.  """
+    one = np.linalg.norm(vector, axis=2)
+    stacked = np.stack((one, one, one), axis=2)
+    return vector / stacked
+
+
+def angle(v1, v2):
+    assert v1.shape == v2.shape
+    product = []
+    for frame in range(v1.shape[0]):
+        product.append([])
+        for residue in range(v2.shape[1]):
+            dot = np.dot(v1[frame, residue, :], v2[frame, residue, :])
+            dot = np.minimum(1.0, dot)
+            dot = np.maximum(-1.0, dot)
+            product[frame].append(dot)
+    product = np.array(product)
+    return np.arccos(product)
+
+
 class AngleDistribution(object):
     """
     A tool that generate order parameter calculations automatically for a
     given topology
     """
     def __init__(self, topology, first_vector, constant_second_vector=True,
-                 second_vector=[0, 0, 1], sep_leaflet=False, recipe=None):
+                 second_vector=[0, 0, 1], sep_leaflet=False, recipe=None,
+                 nbins=90):
         """
         Args:
             topology: the mdtraj topology
             first_vector: the atom pair of the vector
             second_vector: can be a constant vector or another atom pair
         """
+        self.nbins = nbins
         self.atom1 = first_vector[0]
         self.atom2 = first_vector[1]
         self.constant_second_vector = constant_second_vector
@@ -26,15 +49,16 @@ class AngleDistribution(object):
         else:
             self.constant_vector = second_vector
         self.n_frames = 0.0
-        self.distribution = 0.0
         self.topology = topology
         self.sep_leaflet = sep_leaflet
         self.recipe = recipe
         self.sele1 = self.topology.select(self.atom1)
         self.sele2 = self.topology.select(self.atom2)
         if not self.sep_leaflet:
-            pass
+            self.distribution = 0.0
         else:
+            self.distribution_up = 0.0
+            self.distribution_low = 0.0
             self.sele1_up, self.sele1_low = hard_up_low_atoms(
                 self.sele1, self.recipe
             )
@@ -60,17 +84,25 @@ class AngleDistribution(object):
             atom2 = traj.xyz[:, self.sele2]
             vector1 = atom1 - atom2
             if self.constant_second_vector:
-                vector2 = self.constant_vector  # duplicated by number of frames
+                vector2 = np.tile(
+                    self.constant_vector, (vector1.shape[0], vector1.shape[1])
+                )  # duplicated by number of frames and residues
             else:
                 atom3 = traj.xyz[:, self.sele3]
                 atom4 = traj.xyz[:, self.sele4]
                 vector2 = atom3 - atom4
-            return vector1, vector2
             # normalize all the vectors and do the inner product
-            #self.distribution = self.n_frames * self.distribution + \
-            #           distribution * traj.n_frames
-            #self.n_frames += traj.n_frames
-            #self.distribution /= self.n_frames
+            vector1 = unit_vector(vector1)
+            vector2 = unit_vector(vector2)
+            angles = angle(vector1, vector2)
+            distribution, edges = np.histogram(
+                angles, bins=self.nbins, range=(0, np.pi)
+            )
+            self.edges = edges
+            self.distribution = self.n_frames * self.distribution + \
+                distribution * traj.n_frames
+            self.n_frames += traj.n_frames
+            self.distribution /= self.n_frames
         else:
             atom1_up = traj.xyz[:, self.sele1_up]
             atom2_up = traj.xyz[:, self.sele2_up]
@@ -79,6 +111,41 @@ class AngleDistribution(object):
             vector1_up = atom1_up - atom2_up
             vector1_low = atom1_low - atom2_low
             if self.constant_second_vector:
-                vector2_up = self.constant_second_vector
-                vector2_low = self.constant_second_vector
-            # normalize and do inner product
+                vector2_up = np.tile(
+                    self.constant_second_vector, (
+                        vector1_up.shape[0], vector1_up.shape[1]
+                    )  # duplicated by number of frames and residues
+                )
+                vector2_low = np.tile(
+                    -self.constant_second_vector, (
+                        vector1_low.shape[0], vector1_low.shape[1]
+                    )  # duplicated by number of frames and residues
+                )
+            else:
+                atom3_up = traj.xyz[:, self.sele3_up]
+                atom4_up = traj.xyz[:, self.sele4_up]
+                atom3_low = traj.xyz[:, self.sele3_low]
+                atom4_low = traj.xyz[:, self.sele4_low]
+                vector2_up = atom3_up - atom4_up
+                vector2_low = atom3_low - atom4_low
+            # normalize all the vectors and do the inner product
+            vector1_up = unit_vector(vector1_up)
+            vector2_up = unit_vector(vector2_up)
+            vector1_low = unit_vector(vector1_low)
+            vector2_low = unit_vector(vector2_low)
+            angles_up = angle(vector1_up, vector2_up)
+            angles_low = angle(vector1_low, vector2_low)
+            distribution_up, edges = np.histogram(
+                angles_up, bins=self.nbins, range=(0, np.pi)
+            )
+            distribution_low, edges = np.histogram(
+                angles_low, bins=self.nbins, range=(0, np.pi)
+            )
+            self.edges = edges
+            self.distribution_up = self.n_frames * self.distribution_up + \
+                distribution_up * traj.n_frames
+            self.distribution_low = self.n_frames * self.distribution_low + \
+                distribution_low * traj.n_frames
+            self.n_frames += traj.n_frames
+            self.distribution_up /= self.n_frames
+            self.distribution_low /= self.n_frames
