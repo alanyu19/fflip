aecce5b790723d4a0b1aeaa9593070317a5dfbd3
Yalun Yu <alanyu@m3.lobos.nih.gov>
2020-07-23 22:06:58 -0400

    dipolar useful

--- fflip/analysis/dipolar.py
+++ fflip/analysis/dipolar.py
@@ -14,23 +14,14 @@ class DipolarCouplingCalculator:
         self.recipe = recipe
         self.sele1 = self.topology.select(self.atom1)
         self.sele2 = self.topology.select(self.atom2)
-        if not self.sep_leaflet:
-            self.distribution = 0.0
-        else:
-            self.distribution_up = 0.0
-            self.distribution_low = 0.0
-            self.sele1_up, self.sele1_low = hard_up_low_atoms(
-                self.sele1, self.recipe
-            )
-            self.sele2_up, self.sele2_low = hard_up_low_atoms(
-                self.sele2, self.recipe
-            )
-
+        self.traj_values = []
+        
     def __call__(self, traj):
         if not self.sep_leaflet:
             atom1 = traj.xyz[:, self.sele1]
             atom2 = traj.xyz[:, self.sele2]
             vector1 = atom1 - atom2
+            vector1copied = vector1
             vector2 = np.tile(
                 [0, 0, 1],
                 (vector1.shape[0], vector1.shape[1], 1)
@@ -40,9 +31,12 @@ class DipolarCouplingCalculator:
             vector2 = unit_vector(vector2)
             angles = angle(vector1, vector2)
             # angles_normalized = angles / np.pi
-            distances = vector1
-            print(angles.shape)
-            print(distances.shape)
-            #coupling = 12236.5 * np.mean(
-            #    (3 * np.cos(angles)**2 - 1) * distances**(-3) / 2
-            #)
+            distances = np.linalg.norm(vector1copied, axis=2) * 10  # to angstrom
+            np.savetxt('distances.txt', distances)
+            coupling = 12236.5 * np.mean(
+                (3 * np.cos(angles)**2 - 1) * distances**(-3) / 2
+            )
+            self.traj_values.append(np.abs(coupling))
+            self.avg = np.mean(self.traj_values)
+            self.se = np.std(self.traj_values) / np.sqrt(len(self.traj_values))
+
