93b2d592fc8999952b6163d9dacc9719987d1408
anthony-pane <anthony.joel.pane@gmail.com>
2022-09-27 17:24:50 -0400

    LJ Parameters Bug Fix

--- fflip/omm/util.py
+++ fflip/omm/util.py
@@ -462,7 +462,6 @@ change_14=True,parameter_group=None,parameter_offset=None):
     for i, ps in enumerate(parameter_sets)]
     with warnings.catch_warnings():
         warnings.simplefilter("ignore", CharmmPSFWarning)
-    #topology = md.Topology.from_openmm(psfworkflow.psf.topology)
     for g, offset in zip(parameter_sets, all_offsets):
         if g.par_type == 'sigma':
             par_type = 'rmin'
@@ -470,7 +469,7 @@ change_14=True,parameter_group=None,parameter_offset=None):
             par_type = 'epsilon'
         atom_type = None
         for name in g.center_names:
-            atoms = psfworkflow.select("name {}".format(name)) # was topology.select
+            atoms = psfworkflow.select("name {}".format(name))
             first_atom_index = int(atoms[0])
             assert psfworkflow.psf.atom_list[first_atom_index].name == name
             try:
@@ -489,39 +488,40 @@ change_14=True,parameter_group=None,parameter_offset=None):
             if change_14:
                 psfworkflow.parameters.atom_types_str[atom_type].epsilon_14 *= \
                 ( 1 + offset )
-        # include to change specific parameter for energy perturbation,
-        # may need to clean up
-        if parameter_group is not None:
-            if g.par_type == 'sigma':
-                par_type = 'rmin'
-            elif g.par_type == 'epsilon':
-                par_type = 'epsilon'
-            else:
-                raise Exception(
-                    "Parameter type {} not supported by this function".format(
-                        parameter_group.par_type
-                    )
+    # include to change specific parameter for energy perturbation,
+    # may need to clean up
+    if parameter_group is not None:
+        if parameter_group.par_type == 'sigma':
+            par_type = 'rmin'
+        elif parameter_group.par_type == 'epsilon':
+            par_type = 'epsilon'
+        else:
+            raise Exception(
+                "Parameter type {} not supported by this function".format(
+                    parameter_group.par_type
                 )
-            for name in g.center_names:
-                atoms = psfworkflow.select("name {}".format(name)) # was topology.select
-                first_atom_index = int(atoms[0])
-                assert psfworkflow.psf.atom_list[first_atom_index].name == name
-                try:
-                    atom_type = psfworkflow.psf.atom_list[first_atom_index].attype
-                except AttributeError:
-                    raise Exception("Psfworkflow does not have attype attribute")
-            if par_type == 'rmin':
-                psfworkflow.parameters.atom_types_str[atom_type].rmin *= \
+            )
+        atom_type = None
+        for name in parameter_group.center_names:
+            atoms = psfworkflow.select("name {}".format(name))
+            first_atom_index = int(atoms[0])
+            assert psfworkflow.psf.atom_list[first_atom_index].name == name
+            try:
+                atom_type = psfworkflow.psf.atom_list[first_atom_index].attype
+            except AttributeError:
+                raise Exception("Psfworkflow does not have attype attribute")
+        if par_type == 'rmin':
+            psfworkflow.parameters.atom_types_str[atom_type].rmin *= \
+            ( 1 + parameter_offset )
+            if change_14:
+                psfworkflow.parameters.atom_types_str[atom_type].rmin_14 *= \
                 ( 1 + parameter_offset )
-                if change_14:
-                    psfworkflow.parameters.atom_types_str[atom_type].rmin_14 *= \
-                    ( 1 + parameter_offset )
-            elif par_type == 'epsilon':
-                psfworkflow.parameters.atom_types_str[atom_type].epsilon *= \
+        elif par_type == 'epsilon':
+            psfworkflow.parameters.atom_types_str[atom_type].epsilon *= \
+            ( 1 + parameter_offset )
+            if change_14:
+                psfworkflow.parameters.atom_types_str[atom_type].epsilon_14 *= \
                 ( 1 + parameter_offset )
-                if change_14:
-                    psfworkflow.parameters.atom_types_str[atom_type].epsilon_14 *= \
-                    ( 1 + parameter_offset )
 
 
 # Function to change the charge parameters of the atoms
@@ -575,7 +575,7 @@ parameter_group=None,parameter_offset=None):
         center_around='not water'
     )
     change_lj_param(psfworkflow,lipid,solution,change_14=True,
-        parameter_group=parameter_group,parameter_offset=parameter_offset)
+        parameter_group=parameter_group[0],parameter_offset=parameter_offset[0])
     psfworkflow.create_system(
         nonbondedMethod=nonbonded_method,
         constraints=HBonds,
